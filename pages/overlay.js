fa1e017cb2d930db9a7df382835de0d0e3b9c94f
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2021-06-26 13:55:28 +0200

    Cambiata logica visualizzazione punti

--- pages/overlay.js
+++ pages/overlay.js
@@ -8,43 +8,6 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMzMyODQ2MywiZXhwIjoxOTM4OTA0NDYzfQ.qERtQfqDfQF7_ekIPvmfa7YBXswXGOEfQCz0qcUQOp8"
 );
 
-export default function Fusion() {
-  var [partita, setPartita] = useState({});
-  var [tmp, setTmp] = useState(0); // Force update
-  var forceUpdate = () => setTmp((v) => v + 1);
-  useEffect(() => {
-    supabase
-      .from(PARTITE_BOLGHERA)
-      .select("*")
-      .order("id", { ascending: false })
-      .single()
-      .then((r) => setPartita(r.data));
-    supabase
-      .from(PARTITE_BOLGHERA)
-      .on("UPDATE", (r) => setPartita(r.new))
-      .subscribe();
-  }, []);
-  return <UI partita={partita} forceUpdate={forceUpdate} />;
-}
-
-function UI(props) {
-  return (
-    <table className="overlay">
-      <thead></thead>
-      <tbody>
-        <tr>
-          <td>{props.partita.nomeBol}</td>
-          <Set bol partita={props.partita} />
-        </tr>
-        <tr>
-          <td>{props.partita.nomeAvv}</td>
-          <Set partita={props.partita} />
-        </tr>
-      </tbody>
-    </table>
-  );
-}
-
 function reduceSetNormale(obj, keys, bol) {
   return keys.reduce((a, v) => {
     var punti = obj[v].split("-");
@@ -83,23 +46,68 @@ function reduceSetCorto(obj, keys, bol) {
   }, 0);
 }
 
-function getLastPoints(obj, keys, bol) {
-  const lastKey = keys.filter((s) => obj[s] != "0-0").slice(-1, 5)[0];
-  console.log(lastKey);
-  if (!lastKey) return;
-  console.log(obj[lastKey].split("-"));
+export default function Fusion() {
+  var [partita, setPartita] = useState({});
+  var [tmp, setTmp] = useState(0); // Force update
+  var forceUpdate = () => setTmp((v) => v + 1);
+  useEffect(() => {
+    supabase
+      .from(PARTITE_BOLGHERA)
+      .select("*")
+      .order("id", { ascending: false })
+      .single()
+      .then((r) => setPartita(r.data));
+    supabase
+      .from(PARTITE_BOLGHERA)
+      .on("UPDATE", (r) => setPartita(r.new))
+      .subscribe();
+  }, []);
+  partita = { ...partita };
+  console.log("parita.setBol:", partita.setBol);
+  const setKeys = Object.keys(partita).filter((k) => k.startsWith("set"));
+  const set1Bol = reduceSetNormale(partita, setKeys.slice(0, 4), true);
+  const set2Bol = reduceSetCorto(partita, setKeys.slice(4, 5), true);
+  const set1Avv = reduceSetNormale(partita, setKeys.slice(0, 4), false);
+  const set2Avv = reduceSetCorto(partita, setKeys.slice(4, 5), false);
+  console.log("setKeys:", setKeys);
+  partita.setKeys = setKeys;
+  partita.setBol = set1Bol + set2Bol;
+  partita.setAvv = set1Avv + set2Avv;
+  return <UI partita={partita} forceUpdate={forceUpdate} />;
+}
+
+function UI(props) {
+  return (
+    <table className="overlay">
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td>{props.partita.nomeBol}</td>
+          <Set bol partita={props.partita} />
+        </tr>
+        <tr>
+          <td>{props.partita.nomeAvv}</td>
+          <Set partita={props.partita} />
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+function getLastPoints(obj, bol) {
+  const lastKey = obj.setKeys[obj.setBol + obj.setAvv];
+  console.log(obj.setKeys, lastKey);
+  if (!lastKey) return "0";
   return obj[lastKey].split("-")[bol ? 0 : 1];
 }
 
 function Set(props) {
-  var setKeys = Object.keys(props.partita).filter((k) => k.startsWith("set"));
-  const set1 = reduceSetNormale(props.partita, setKeys.slice(0, 4), props.bol);
-  const set2 = reduceSetCorto(props.partita, setKeys.slice(4, 5), props.bol);
-  const set = set1 + set2;
+  console.log("setKeys2:", props.partita.setKeys);
+  const set = props.bol ? props.partita.setBol : props.partita.setAvv;
   return (
     <>
       <td>{set}</td>
-      <td>{getLastPoints(props.partita, setKeys, props.bol)}</td>
+      <td>{getLastPoints(props.partita, props.bol)}</td>
     </>
   );
 }
