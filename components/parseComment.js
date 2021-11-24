7a8b0a5f2f84d0840be7151a2f27abde77485679
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2021-11-24 23:44:18 +0100

    Create parseComment.js

---
+++ components/parseComment.js
@@ -0,0 +1,21 @@
+export default function parseComment(data) {
+  if (!data) return null;
+  const commento = data.note.Commento.split(";");
+  var nomeCasa, nomeOspiti;
+  var liberoCasa, liberoOspiti;
+  if (commento[2] && commento[2] != "no comments") {
+    var commento2 = commento[2].split(",");
+    nomeCasa = commento2[0];
+    liberoCasa = commento2.length >= 2 && commento2[1];
+  } else {
+    nomeCasa = data.note.SqInDesC;
+  }
+  if (commento[3] && commento[3] != "no comments") {
+    var commento2 = commento[3].split(",");
+    nomeOspiti = commento2[0];
+    liberoOspiti = commento2.length >= 2 && commento2[1];
+  } else {
+    nomeOspiti = data.note.SqOsDesC;
+  }
+  return { nomeCasa, nomeOspiti, liberoCasa, liberoOspiti };
+}
