9bd5761f0632ae4cfe8bb62ac3e2130465ebac7a
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2021-11-25 13:30:55 +0100

    Pulizia

--- pages/sestetto.js
+++ pages/sestetto.js
@@ -17,20 +17,20 @@ function UI(props) {
   return (
     <div className="sestetto">
       <div style={{ marginLeft: "30px" }}>
-        <div className="nome-squadra">{props.nomeCasa}</div>
-        <Quadrato bolghera data={props.data} />
+        <div className="nome-squadra">{note.SqInDesC}</div>
+        <Quadrato bolghera {...props} />
       </div>
       <div style={{ marginRight: "30px" }}>
-        <Allenatore bolghera data={props.data} />
-        <Libero bolghera data={props.data} libero />
+        <Allenatore bolghera {...props} />
+        <Libero bolghera {...props} libero />
       </div>
       <div style={{ marginLeft: "30px" }}>
-        <Allenatore data={props.data} />
-        <Libero data={props.data} libero />
+        <Allenatore {...props} />
+        <Libero {...props} libero />
       </div>
       <div style={{ marginRight: "30px" }}>
-        <div className="nome-squadra">{props.nomeOspiti}</div>
-        <Quadrato data={props.data} />
+        <div className="nome-squadra">{note.SqOsDesC}</div>
+        <Quadrato {...props} />
       </div>
     </div>
   );
@@ -39,8 +39,8 @@ function UI(props) {
 function Quadrato(props) {
   return (
     <div className={"quadrato " + (props.bolghera ? "bolghera" : "")}>
-      <TreMetri data={props.data} bolghera={props.bolghera} />
-      <SeiMetri data={props.data} bolghera={props.bolghera} />
+      <TreMetri {...props} bolghera={props.bolghera} />
+      <SeiMetri {...props} bolghera={props.bolghera} />
     </div>
   );
 }
@@ -48,9 +48,9 @@ function Quadrato(props) {
 function TreMetri(props) {
   return (
     <div className="tre-metri">
-      <Persona data={props.data} n="4" bolghera={props.bolghera} />
-      <Persona data={props.data} n="3" bolghera={props.bolghera} />
-      <Persona data={props.data} n="2" bolghera={props.bolghera} />
+      <Persona {...props} n="4" bolghera={props.bolghera} />
+      <Persona {...props} n="3" bolghera={props.bolghera} />
+      <Persona {...props} n="2" bolghera={props.bolghera} />
     </div>
   );
 }
@@ -58,9 +58,9 @@ function TreMetri(props) {
 function SeiMetri(props) {
   return (
     <div className="sei-metri">
-      <Persona data={props.data} n="5" bolghera={props.bolghera} />
-      <Persona data={props.data} n="6" bolghera={props.bolghera} />
-      <Persona data={props.data} n="1" bolghera={props.bolghera} />
+      <Persona {...props} n="5" bolghera={props.bolghera} />
+      <Persona {...props} n="6" bolghera={props.bolghera} />
+      <Persona {...props} n="1" bolghera={props.bolghera} />
     </div>
   );
 }
@@ -81,8 +81,12 @@ function Persona(props) {
         ).trim()
     );
   const NPall = data.note[bolghera ? "ZPagg0" : "ZPagg1"];
+  const classes = ["persona"];
+  if (NPall == props.n) {
+    classes.push("pall");
+  }
   return (
-    <div className={"persona " + (NPall == props.n ? "pall" : "")}>
+    <div className={classes.join(" ")}>
       <div className="numero">{giocatore.Pet}</div>
       <div className="cognome">{capitalize(giocatore.Cognome)}</div>
     </div>
@@ -117,6 +121,7 @@ function Libero(props) {
       .filter((p) => p.CodSq == (bolghera ? "0" : "1"))
       .find((p) => p.IDRuolo == "1");
   }
+  if (!giocatore) return null;
   return (
     <div className="libero">
       <div className="numero">{giocatore.Pet}</div>
