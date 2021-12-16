ba80f89e4ae7af271228c2c28e41d5c6d8e56118
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2021-12-16 11:22:16 +0100

    Aggiunto title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.scss";
 
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Bolghera</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
