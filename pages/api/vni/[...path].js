13b12c2b4e0d8ba97dd2e2287163e88dc9d084dd
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2022-10-05 20:49:46 +0200

    Add proxy

---
+++ pages/api/vni/[...path].js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  if (req.method != "GET") {
+    res.status(404).end();
+    return;
+  }
+  if (/[^a-zA-Z0-9?_/=&]/.test(req.url)) {
+    res.status(401).end();
+    return;
+  }
+  return fetch("http://volleynetworkitalia.it" + req.url.substring(8))
+    .then((r) => {
+      if (r.ok) return r.json();
+      throw r;
+    })
+    .then((d) => res.status(200).json(d))
+    .catch((e) => {
+      console.error(e);
+      res.status(500).end();
+    });
+}
